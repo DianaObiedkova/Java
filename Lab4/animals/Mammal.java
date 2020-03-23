60d1deaf1e677a159f30b5ea1a6d34b83b0e0c64
DianaObiedkova <54158045+DianaObiedkova@users.noreply.github.com>
2020-03-23 14:26:50 +0200

    Add files via upload

---
+++ Lab4/animals/Mammal.java
@@ -0,0 +1,18 @@
+package animals;
+
+public abstract class Mammal extends Animal{
+    private mammalType mamType;
+
+    public Mammal(int id, int age, double weight, mammalType mamtype){
+        super(id, age, weight);
+        this.mamType=mamType;
+    }
+
+    public mammalType getMamType() {
+        return mamType;
+    }
+
+    public void setMamType(mammalType mamType) {
+        this.mamType = mamType;
+    }
+}
