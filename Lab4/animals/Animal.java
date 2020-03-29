ec4d6b58995a976efcd908bb6833089f2575aaf4
DianaObiedkova <54158045+DianaObiedkova@users.noreply.github.com>
2020-03-29 17:23:37 +0300

    Add files via upload

---
+++ Lab4/animals/Animal.java
@@ -0,0 +1,37 @@
+package animals;
+
+public abstract class Animal {
+    private int id;
+    private int age;
+    private double weight;
+
+    public Animal(int id, int age,double weight){
+        this.id=id;
+        this.age=age;
+        this.weight=weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
