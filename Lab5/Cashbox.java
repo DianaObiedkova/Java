d61e8310bf3d1978da8de89ab58b258b59350444
DianaObiedkova <54158045+DianaObiedkova@users.noreply.github.com>
2020-03-31 14:24:35 +0300

    Update Cashbox.java

--- Lab5/Cashbox.java
+++ Lab5/Cashbox.java
@@ -63,7 +63,6 @@ public class Cashbox {
 
         products.clear();
         products.putAll(sortedMap);
-        //showProducts(products);
     }
 
     private void sortKey(){
@@ -77,127 +76,9 @@ public class Cashbox {
         var wrapper=new Object(){String text="";};
         sortedProducts.forEach((k,v)->{wrapper.text +=k.getName()+"\t"+v+"\n";} );
         return wrapper.text;
-       // pros.forEach((k,v)->System.out.println(k.getName()+"\t"+v+"\n"));
     }
 
     private void resetList(Map<Product,Integer> pros){
         pros.clear();
     }
 }
-
-//products.sort(Comparator.comparing(Product::getName)
-//    .thenComparing(Product::getAmount));
-
-
-/*
-                List<Product> temp=products;
-                temp.stream()
-                        //.filter(p->p.equals(product))
-                        .forEach(p->{
-                            if(p.getName()==product.getName())
-                                p.setAmount(p.getAmount()+product.getAmount());});
-                products.remove(pro);
-                products.addAll(temp);
-
-                */
-
-/*
-        if(!products.contains(product))
-             products.add(product);
-        else{
-            products.stream()
-                    //.filter(product1 -> product1.getName()==product.getName())
-                    .forEach(p->{
-                        if(p.getName()==product.getName())
-                            p.setAmount(p.getAmount()+product.getAmount());});
-        }
-
- */
-
-
-//Sorting by Key
-
-
-/*
-        List<Product> temp=products;
-        temp.stream()
-                //.filter(p->p.equals(product))
-                .forEach(p->{
-                    if(p.getName()==product.getName())
-                        p.setAmount(p.getAmount()+product.getAmount());});
-        products.remove(pro);
-        products.addAll(temp);
-
-
-
- */
-
-//-------------
-//Properties properties = System.getProperties();
-//Set<Map.Entry<Object, Object>> entries = properties.entrySet();
-
-//ArrayList<Product> sortedkeys=new ArrayList<Product>(products.keySet());
-//Collections::sort(sortedkeys)
-
-
-//LinkedHashMap<Product,Integer> sortedmap=new LinkedHashMap<>();
-          /*      .collect(Collectors.toMap(k -> (String) k.getKey(), e -> (String) e.getValue()))
-                .entrySet()
-                .stream()
-                .sorted(Entry.comparingByKey())
-                .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-
-         */
-          /*
-        products.entrySet()
-                .stream()
-                .sorted(comparingByKey(Comparator.comparing(obj->obj.getName())))
-                .forEachOrdered(x->sortedmap.put(x.getKey(),x.getValue()));
-
-
-           */
-/*
-       Map<Product,Integer> proMap=products.stream()
-               .collect(Collectors.toMap(Product::getName, Function.identity()));
-        TreeMap<Product, Integer> sorted = new TreeMap<>();
-*/
-//sortedmap.putAll(products);
-
-
-// rollCall.forEach((k,v)->System.out.println(k.getName()+"\t"+v+"\n"));
-
-//products.putAll(rollCall);
-/*
-        products.forEach((k,v)->{
-            if(k.equals(product)) {
-                addValue(products,product,count);
-                //products.put(k,v+count);
-                //v=v+count;
-                System.out.println(v);
-               // products.put(product,v+count);
-
-                //showProducts();
-                return;
-            }
-        });
-
-
- */
-// showProducts();
-        /*
-        for(var pro:products){
-            if(pro.equals(product)){
-               pro.setAmount(pro.getAmount()+product.getAmount());
-                return;
-            }
-        }
-        products.add(product);
-
-         */
-/*
-        for(int i=0; i<products.size();i++){
-            if(products.get(produc))
-        }
-
- */
