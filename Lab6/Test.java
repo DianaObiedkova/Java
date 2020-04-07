683ea99d7a6e1f39d165a4e0b70612442bd5884c
DianaObiedkova <54158045+DianaObiedkova@users.noreply.github.com>
2020-04-07 11:01:00 +0300

    Update Test.java

--- Lab6/Test.java
+++ Lab6/Test.java
@@ -6,8 +6,6 @@ public class Test {
 
     static String fileName = "F:\\Programs\\Projects\\java\\lab6\\src\\test.txt";
     static byte key_sign = 7;
-    static File file;
-    static FileReader fr;
 
     public static void main(String args[]) throws FileNotFoundException {
         try {
@@ -15,8 +13,8 @@ public class Test {
             //String s = "possible result is: 123";
             String s=input();
 
-            coder_writer(s);
-            decoder_reader(file,fr);
+            writer(s);
+            reader(file);
 
 
             } catch (Exception e) {
@@ -28,39 +26,52 @@ public class Test {
     private static String input(){
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter a number:");
+        System.out.println("Enter a string:");
         String num = scan.nextLine();
         scan.close();
         return num;
     }
 
-    private static void coder_writer(String str) throws IOException {
-        FileOutputStream fout = new FileOutputStream(fileName);
+    public static byte[] coder(String str){
         byte b[] = str.getBytes();
 
         for (int i = 0; i < b.length; i++) {
             b[i]+=key_sign;
         }
-
+        return b;
+    }
+    
+    private static void writer(String str) throws IOException {
+        FileOutputStream fout = new FileOutputStream(fileName);
+  
         fout.write(b);
         fout.close();
     }
+    
+    public static String decoder(byte[] bytes){
 
-    private static void decoder_reader(File file, FileReader fr) throws IOException {
+        String str="";
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] -= key_sign;
+            System.out.print((char) bytes[i]);
+            str+=(char) bytes[i];
+        }
 
-        fr = new FileReader(fileName);
-        file = new File(fileName);
-        FileInputStream fileInputStream = null;
+        return str;
+    }
+    
+    private static void decoder_reader() throws IOException {
+
+        File file = new File(fileName);
         byte[] bFile = new byte[(int) file.length()];
         try {
-            //convert file into array of bytes
-            fileInputStream = new FileInputStream(file);
+            
+            FileInputStream fileInputStream = new FileInputStream(file);
             fileInputStream.read(bFile);
             fileInputStream.close();
-            for (int i = 0; i < bFile.length; i++) {
-                bFile[i] -= key_sign;
-                System.out.print((char) bFile[i]);
-            }
+            
+            decoder(bFile);
+            
     } catch (IOException e) {
             e.printStackTrace();
         }
